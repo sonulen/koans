7db2861b2a7f44c1079ae929060bef50c51bd4f1
sonulen <sonulen@gmail.com>
2021-08-12 23:19:32 +0300

    wip collections section

--- Collections/FlatMap/src/Task.kt
+++ Collections/FlatMap/src/Task.kt
@@ -1,6 +1,6 @@
 // Return all products the given customer has ordered
 fun Customer.getOrderedProducts(): List<Product> =
-        TODO()
+        orders.flatMap {  }
 
 // Return all products that were ordered by at least one customer
 fun Shop.getOrderedProducts(): Set<Product> =
